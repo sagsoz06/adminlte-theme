f8ddf0309b641040ee7cade27cbc5a0411798ffe
aniliso <tkaptan41@gmail.com>
2017-07-19 01:11:28 +0300

    Added russian support

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -4,8 +4,4 @@ $(document).ready(function () {
 	});
 
     $(document).ajaxStart(function() { Pace.restart(); });
-
-    Mousetrap.bind('f1', function() {
-        window.open('https://asgardcms.com/docs', '_blank');
-    });
 });
